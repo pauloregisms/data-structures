e7dd07a9fa9f630b8af33555b6e6c480ed925e63
Paulo Regis <pauloregisms@gmail.com>
2020-04-26 16:23:36 -0300

    Altercoes na defiicao de grafo implementada

--- Graph.h
+++ Graph.h
@@ -1,14 +1,21 @@
 typedef struct Graph Graph;
+typedef struct Vertex Vertex;
+typedef struct Edge Edge;
 
-Graph *Graph_create();
-int Graph_delete(Graph *graph);
+Graph  *Graph_create();
 
-int Graph_insert(Graph *graph, void *value);
-void *Graph_find(Graph *graph, void *value, int (*compare)(void*,void*));
-void *Graph_remove(Graph *graph, void *value, int (*compare)(void*,void*));
+Vertex *Graph_findVertex(Graph *graph, void *value);
+Edge *Graph_findEdge(Graph *graph, void *value);
+
+int Graph_insertVertex(Graph *graph, void *value);
+int *Graph_removeVertex(Graph *graph, void *value, int (*compare)(void*,void*));
+
+int Graph_insertEdge(Vertex *origin, Vertex *target, void *value);
+void *Graph_removeEdge(Graph *graph, void *value, int (*compare)(void*,void*));
 
 int Graph_getLength(Graph *graph);
-void Graph_print(Graph *graph, void printValue(void*));
+int Graph_getVertexDegre(Vertex *vertex);
+int Graph_getVertexIncidence(Vertex *vertex);
 
-int Graph_createEdge(Graph *graph, void *nodeOrigin, void *nodeTarget);
-int Graph_removeEdge(Graph *graph, void *nodeOrigin, void *nodeTarget);
+int Graph_print(Graph *graph, void printValue(void*));
+int Graph_delete(Graph *graph);
